a70328d1195310d0d92c5cc1259ef40cd62bf54e
Ella <ellawu010@gmail.com>
2024-09-09 13:48:04 +1200

    add function:button

---
+++ src/app/components/button.js
@@ -0,0 +1,14 @@
+const Button = ({onClick, className, children, ...porps}) => {
+
+    const baseStyle = "text-pink-600 text-xs border-b-[1px] border-solid sborder-current pb-[2px] inline-block bg-transparent cursor-pointer transition-all during-300 hover:text-gray-600 focus:outline-none focus:pulsate-animation"
+        
+    const classNames = `${baseStyle} ${className}`;
+
+    return (
+        <button onClick={onClick} className={classNames} {...porps}>
+            {children}
+        </button>
+    )
+}
+
+export default Button
