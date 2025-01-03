a2cd0fa43fe773550d98503a70ba56c69362dcd3
Ella <ellawu010@gmail.com>
2025-01-03 15:39:21 +0800

    add amplify

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,12 @@
+// layout.js
+import { Inter } from "next/font/google"
+import "./globals.css"
+import React from "react"
 import localFont from "next/font/local";
-import "./globals.css";
+import Providers from "./provider";
+
+const inter = Inter({ subsets:  ["latin"] })
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,11 +27,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
+        <Providers>
         {children}
+        </Providers>
       </body>
     </html>
-  );
+  )
 }
