4cc0a281632d860409c5358edf72334a0037fae0
Ella <ellawu010@gmail.com>
2025-01-08 16:19:12 +0800

    update booking page

--- src/app/components/searchbar.js
+++ src/app/components/searchbar.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import nextConfig from "../../../next.config.mjs";
 import Link from "next/link";
 import DateInput from "./dateinput";
@@ -10,10 +11,13 @@ const SearchBar = () => {
   const today = date.toISOString().split("T")[0];  
   const tomorrow = new Date(date.getTime() + 86400000).toISOString().split("T")[0];  
 
+  const router = useRouter();
+
   const [location, setLocation] = useState("");
   const [checkIn, setCheckIn] = useState(today);
   const [checkOut, setCheckOut] = useState(tomorrow);
   const [guests, setGuests] = useState(1);
+  const [errorMessage, setErrorMessage] = useState('')
 
   const queryString = new URLSearchParams({
     location,
@@ -22,44 +26,58 @@ const SearchBar = () => {
     guests,
   }).toString();
 
+  const handleSearch = () => {
+    // Check if the location is empty
+    if (!location.trim()) {
+      setErrorMessage('Please fill in the Destination.');
+    } else {
+      setErrorMessage(''); // Clear any existing error messages
+      router.push(`/booking?${queryString}`)
+    }
+  };
+
   return (
+    <div className="flex flex-col">
+      
+    
     <div className="flex item-center justify-center bg-white border shadow-lg text-xs rounded-full w-full max-w-2xl mx-auto">
-      <div className="flex justify-between item-center w-full">
-        <div className="p-2 pl-8 rounded-full cursor-pointer hover:bg-gray-100">
+      <div className="flex justify-between text-gray-700 item-center  w-full">
+        <div className="flex flex-col justify-center p-2 pl-8 rounded-full cursor-pointer hover:bg-gray-100">
           <label className="block text-black ">Where</label>
           <input
             type="text"
             placeholder="Destination"
-            className="w-full bg-transparent hover:text-gray-700 focus:outline-none"
+            className="w-full bg-transparent focus:outline-none"
             value={location}
             onChange={(e) => setLocation(e.target.value)}
           />
+         
         </div>
 
-        <div className="p-2 pl-8 rounded-full cursor-pointer hover:bg-gray-100">
+        <div className="flex flex-col justify-center p-2 pl-8 rounded-full cursor-pointer hover:bg-gray-100">
           <label className="block text-black">Chech in</label>
           <DateInput value = {checkIn} minDate={today} onChange={setCheckIn} />
         </div>
 
-        <div className="p-2 pl-8 rounded-full cursor-pointer hover:bg-gray-100">
+        <div className="flex flex-col justify-center p-2 pl-8 rounded-full cursor-pointer hover:bg-gray-100">
           <label className="block text-black">Chech Out</label>
           <DateInput value = {checkOut} minDate={checkIn || today} onChange={setCheckOut} />
         </div>
 
-        <div className="p-2 px-8 rounded-full cursor-pointer hover:bg-gray-100">
+        <div className="flex flex-col justify-center p-2 px-8 rounded-full cursor-pointer hover:bg-gray-100">
           <label className="block text-black">Guests</label>
           <input
             type="number"
             placeholder="1 Adult"
-            className="w-full bg-transparent text-gray-700 hover:text-gray-700 max-w-12 focus:outline-none"
+            className="w-full bg-transparent text-gray-700 max-w-12 focus:outline-none"
             value={guests}
             onChange={(e) => setGuests(e.target.value)}
           />
         </div>
 
         <div className="p-2">
-          <Link href={`/booking?${queryString}`}>
             <button
+               onClick={handleSearch}
               className="bg-pink-600  p-2 text-white rounded-full shadow-sm hover:bg-pink-700"
             >
               <svg className="h-5 w-5 fill-white">
@@ -68,9 +86,12 @@ const SearchBar = () => {
                 ></use>
               </svg>
             </button>
-          </Link>
         </div>
+        
       </div>
+      
+    </div>
+    {errorMessage && <p className="text-xs pl-8 pt-2 text-white-500">{errorMessage}</p>}
     </div>
   );
 };
--- src/app/layout/bookrightpanel.js
+++ src/app/layout/bookrightpanel.js
@@ -2,7 +2,7 @@ import ButtonGroup from "../components/pilltable";
 import HotelCard from "../components/card/hotelcard";
 import nextConfig from "../../../next.config.mjs";
 
-export default function RightPanel() {
+export default function RightPanel({hotels}) {
   return (
   <div className="flex flex-col basis-4/5 mx-10 mt-8 text-sm font-normal text-gray-600 dark:text-gray-300">
     <div className="flex justify-between items-center mb-8">
@@ -10,15 +10,21 @@ export default function RightPanel() {
       <ButtonGroup />
     </div>
     <div className="grid grid-cols-3 gap-10 mx-10">
-      <HotelCard
-        cardPhoto={`${nextConfig.basePath}/img/hotel-1.jpg`}
-        cardAddress="Groveland, California"
-        cardRate="4.91"
-        cardDetailAddress="Yosemite National Park"
-        cardPrice="$289"
-      />
 
-      <HotelCard
+      {hotels.map((hotel, i) => (
+        <HotelCard
+          key={i}
+          cardPhoto={`${nextConfig.basePath}/img/hotel-1.jpg`}
+          cardAddress={hotel.address}
+          cardRate={hotel.totalRating}
+          cardHotelName={hotel.name}
+          cardPrice={hotel.bookings[0].price}
+          hotelId={hotel.hotelId}
+        />
+      ))}
+      
+
+      {/* <HotelCard
         cardPhoto={`${nextConfig.basePath}/img/hotel-2.jpg`}
         cardAddress="Groveland, California"
         cardRate="4.91"
@@ -96,7 +102,7 @@ export default function RightPanel() {
         cardRate="4.91"
         cardDetailAddress="Yosemite National Park"
         cardPrice="$289"
-      />
+      /> */}
     </div>
   </div>
   );
