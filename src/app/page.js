808e56fa9939b038828d5d2ae0b2c7b584e54155
Ella <ellawu010@gmail.com>
2025-01-08 16:18:16 +0800

    update home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,33 @@
+"use client"
+
 import Header from "./layout/header";
 import SearchBar from "./components/searchbar";
 import HomeContent from "./layout/homecontent";
+import useSWR from "swr"
+import { useState } from "react";
+
+const fetcher = async (url) => {
+  const response = await fetch(url)
+  if(!response.ok) {
+    throw new Error('API request failed')
+  }
+  return response.json()
+}
 
 export default function Home() {
+
+  const { data: hotels, error, isLoading } = useSWR('http://localhost:5260/api/hotels/', fetcher)
+  const [searchQuery, setSearchQuery] = useState('')
+
+  // console.log(JSON.stringify(data, null, 2)); 
+
+  if (error) return <div>failed to load: {error.message}</div>
+  if (isLoading) 
+    return  
+      <div className="flex justify-center p-4">
+        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+      </div>
+      
   return (
     <div>
       <div className="bg-white shadow-xl">
@@ -23,10 +48,11 @@ export default function Home() {
                 Source code
             </a>
           </div>
-          <SearchBar />
+          <SearchBar onSearch={setSearchQuery}/>
+          
         </div>
 
-        <HomeContent />    
+        <HomeContent hotels={hotels} searchQuery={searchQuery}/>    
              
       </div>
     </div>
